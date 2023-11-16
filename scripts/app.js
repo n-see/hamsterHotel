453ae4161d92f3a1c28e10083652b527618d4e65
Neo See <neococo123@yahoo.com>
2023-11-15 22:09:10 -0800

    Worked on Resopsiveness/updated app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,15 @@
 let clickMe = document.getElementById("clickMe");
+let checkIn = document.getElementById("checkIn");
+let checkOut = document.getElementById("checkOut");
+let guest = document.getElementById("guest");
+let roomType = document.getElementById("roomType");
 
+clickMe.addEventListener("click", function() {
+    alert("Access Denied :( ");
+    checkIn.innerText = " ";
+    checkOut.innerText = " ";
+    guest.innerText = " ";
+    roomType.innerText = " ";
+});
+
+console.log(checkIn, checkOut, guest, roomType);
